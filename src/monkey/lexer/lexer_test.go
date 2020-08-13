62773062670782447def2c8ab15e95571c139301
Naruhiro Hayashi <naruhiro_hayashi@so-netmedia.jp>
2020-08-13 20:28:47 +0900

    add lexer

--- src/monkey/lexer/lexer_test.go
+++ src/monkey/lexer/lexer_test.go
@@ -9,7 +9,7 @@ func TestNextToken(t *testing.T) {
 	input := `=+(){},;`
 
 	tests := []struct {
-		expectType      token.TokenType
+		expectedType    token.TokenType
 		expectedLiteral string
 	}{
 		{token.ASSIGN, "="},
